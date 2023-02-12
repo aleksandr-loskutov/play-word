8880a5f25f84f05c7e5880a58052e86b3a45dc27
Aleksandr <loskutov@aleksandrl.ru>
2023-02-12 22:09:56 +0300

    Add new endpoints

--- packages/server/src/collection/collection.controller.ts
+++ packages/server/src/collection/collection.controller.ts
@@ -1,17 +1,21 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Param,
   Post,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { AtGuard } from '../common/guards';
 import { CollectionService } from './collection.service';
 import { GetCurrentUserId } from '../common/decorators';
 import { Collection } from '@prisma/client';
-import { CollectionDto } from './dto';
+import { RequestCollectionCreate, RequestCollectionUpdate } from './dto';
+import { Response } from 'common';
 
 @UseGuards(AtGuard)
 @Controller('collections')
@@ -21,13 +25,49 @@ export class CollectionController {
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
   createCollection(
-    @Body() newCollection: CollectionDto,
+    @Body() payload: RequestCollectionCreate,
     @GetCurrentUserId() userId: number,
-  ): Promise<Collection> {
-    return this.collectionService.createCollection(newCollection, userId);
+  ): Promise<Response<Collection>> {
+    return this.collectionService.createCollection(payload, userId);
   }
+
+  @Put('/:collectionId')
+  @HttpCode(HttpStatus.OK)
+  updateCollection(
+    @Body() payload: RequestCollectionUpdate,
+    @Param('collectionId') collectionId: string,
+    @GetCurrentUserId() userId: number,
+  ): Promise<Response<Collection>> {
+    return this.collectionService.updateCollection(
+      parseInt(collectionId),
+      userId,
+      payload,
+    );
+  }
+
   @Get('/')
-  getUserCollections(@GetCurrentUserId() userId: number) {
+  @HttpCode(HttpStatus.OK)
+  getUserCollections(
+    @GetCurrentUserId() userId: number,
+  ): Promise<Response<Collection[]>> {
     return this.collectionService.getUserCollections(userId);
+  }
+
+  @Delete('/:collectionId')
+  @HttpCode(HttpStatus.OK)
+  deleteCollection(
+    @Param('collectionId') collectionId: string,
+    @GetCurrentUserId() userId: number,
+  ): Promise<Response<Collection>> {
+    return this.collectionService.deleteCollection(
+      parseInt(collectionId),
+      userId,
+    );
+  }
+
+  @Get('/public')
+  @HttpCode(HttpStatus.OK)
+  getPublicCollections(): Promise<Response<Collection[]>> {
+    return this.collectionService.getPublicCollections();
   }
 }

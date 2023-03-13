91332d9f8b2b3fb1603c118332dae5fd48285e9f
Aleksandr <loskutov@aleksandrl.ru>
2023-03-13 21:54:34 +0300

    Refactor addWords method

--- packages/server/src/word/word.service.ts
+++ packages/server/src/word/word.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { WordDto } from './dto';
-import { CollectionWordDto } from '../collection/dto';
+import { Collection } from '@prisma/client';
+import { Response } from 'common';
 
 @Injectable()
 export class WordService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getWordsByCollection(collectionId: number): Promise<WordDto[]> {
-    const wordsIds = await this.prisma.collectionWord.findMany({
+    const wordsIds = await this.prisma.wordForCollection.findMany({
       where: {
         collectionId,
       },
@@ -30,7 +31,9 @@ export class WordService {
   async addWords(words: WordDto[]): Promise<WordDto[]> {
     const uniqueWords = Array.from(
       new Set(words.map((word) => JSON.stringify(word))),
-    ).map((word) => JSON.parse(word));
+    )
+      .map((word) => JSON.parse(word))
+      .filter((word) => Object.keys(word).length > 0);
 
     const newWords = await Promise.all(
       uniqueWords.map(async (word) => {
@@ -59,34 +62,49 @@ export class WordService {
   async addWordsToCollection(
     collectionId: number,
     words: WordDto[],
-  ): Promise<CollectionWordDto[]> {
-    const addedWords = await this.addWords(words);
-    const existingWordsInCollection = await this.prisma.collectionWord.findMany(
-      {
-        where: {
+  ): Promise<Response<Collection>> {
+    try {
+      const addedWords = await this.addWords(words);
+      const existingWordsInCollection =
+        await this.prisma.wordForCollection.findMany({
+          where: {
+            collectionId,
+            wordId: { in: addedWords.map((word) => word.id) },
+          },
+          select: { wordId: true },
+        });
+      const newWords = addedWords.filter(
+        (word) =>
+          !existingWordsInCollection.some(
+            (existingWord) => existingWord.wordId === word.id,
+          ),
+      );
+      await this.prisma.wordForCollection.createMany({
+        data: newWords.map((word) => ({
           collectionId,
-          wordId: {
-            in: addedWords.map((word) => word.id),
+          wordId: word.id,
+        })),
+      });
+
+      const collection = await this.prisma.collection.findUnique({
+        where: { id: collectionId },
+        include: {
+          words: {
+            select: {
+              word: true,
+            },
           },
         },
-      },
-    );
-    const newWords = addedWords.filter((word) => {
-      return !existingWordsInCollection.find(
-        (existingWord) => existingWord.wordId === word.id,
+      });
+      return (
+        collection || {
+          error: `Failed to add words to collection with id ${collectionId}`,
+        }
       );
-    });
-
-    const result = await this.prisma.$transaction(
-      newWords.map((word) =>
-        this.prisma.collectionWord.create({
-          data: {
-            collectionId,
-            wordId: word.id,
-          },
-        }),
-      ),
-    );
-    return result;
+    } catch (error: any) {
+      return {
+        error: `Cannot add words to collection with id "${collectionId}". ${error.message}`,
+      };
+    }
   }
 }

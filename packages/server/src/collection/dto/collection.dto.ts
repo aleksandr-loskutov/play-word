65cee17c6782918a6d4c024510dc7d07f1f0b57e
Aleksandr <loskutov@aleksandrl.ru>
2023-03-13 21:52:52 +0300

    Add dto classes

--- packages/server/src/collection/dto/collection.dto.ts
+++ packages/server/src/collection/dto/collection.dto.ts
@@ -1,13 +1,61 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Collection, Word } from '@prisma/client';
 
 export class CollectionDto {
   @IsNotEmpty()
+  @IsString()
   name: string;
+  @IsString()
+  description: string;
+  @IsString()
+  image: string;
+  @IsNumber()
+  userId: number;
+  @IsBoolean()
+  isPublic: boolean;
+}
+
+export class RequestCollectionCreate {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsString()
+  @IsOptional()
+  description?: string;
+  @IsString()
+  @IsOptional()
+  image?: string;
+  @IsBoolean()
+  @IsOptional()
+  isPublic?: boolean;
+}
+
+export class RequestCollectionUpdate extends RequestCollectionCreate {
+  @IsOptional()
+  @IsString()
+  override name: string;
+}
+
+export class RequestLearnCollection {
+  @IsOptional()
+  @IsObject()
+  options?: any;
 }
 
 export class CollectionWordDto {
   @IsNotEmpty()
+  @IsNumber()
   collectionId: number;
   @IsNotEmpty()
+  @IsNumber()
   wordId: number;
 }
+
+export type CollectionWithWords = Collection & { words: Word[] };

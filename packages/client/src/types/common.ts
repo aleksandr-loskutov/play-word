98f8b2547c17486b87be5247009b939403cbafa5
Aleksandr <loskutov@aleksandrl.ru>
2023-03-20 22:52:07 +0300

    update types

--- packages/client/src/types/common.ts
+++ packages/client/src/types/common.ts
@@ -1,2 +1,3 @@
 export type Nullable<T> = T | null
 export type Indexable<T> = { [key: string]: T }
+export type EntityId = { id: number }
--- packages/client/src/types/training.ts
+++ packages/client/src/types/training.ts
@@ -1,8 +1,14 @@
+import { WordForCollection } from './collection'
+import { EntityId } from './common'
+
 export type UserWordProgress = {
   wordId: number
   stage: number
   nextReview: Date
+  word: Word
 }
+
+export type Word = WordForCollection & EntityId
 
 export type RequestUserWordProgressUpdate = {
   wordId: number

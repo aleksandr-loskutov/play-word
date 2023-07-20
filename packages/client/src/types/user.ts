de50b20a7c1930df919d2e8dfe2aaeeb880a13e3
Aleksandr <loskutov@aleksandrl.ru>
2023-07-20 14:52:51 +0300

    Add types for api and user

---
+++ packages/client/src/types/user.ts
@@ -0,0 +1,32 @@
+export type User = {
+  id: number
+  name: string
+  email: string
+  createdAt: string
+  trainingSettings: UserTrainingSettings
+}
+
+export type RequestUserData = Omit<User, 'id' | 'createdAt'>
+
+export type PasswordChangeFormData = {
+  password: string | undefined
+  passwordRepeat: string | undefined
+}
+
+export type RequestUserDataUpdate = RequestUserData & PasswordChangeFormData
+
+export type UserTrainingSettings = {
+  userId: number
+  wordsPerSession: number
+  stageOneInterval: number
+  stageTwoInterval: number
+  stageThreeInterval: number
+  stageFourInterval: number
+  stageFiveInterval: number
+  countdownTimeInSec: number
+  useCountdown: boolean
+  strictMode: boolean
+  showCollectionNameHint: boolean
+  wordErrorLimit: number
+  wordMistypeLimit: number
+}

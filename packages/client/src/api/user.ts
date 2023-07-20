b0f2e1d631b1aa526c2dbb580bb822d313d745db
Aleksandr <loskutov@aleksandrl.ru>
2023-07-20 14:45:07 +0300

    Add user api

---
+++ packages/client/src/api/user.ts
@@ -0,0 +1,25 @@
+import { ApiResponse } from '../types/api'
+import { User, RequestUserDataUpdate } from '../types/user'
+import BaseAPI from './base'
+
+class UserAPI extends BaseAPI {
+  constructor() {
+    super('/user')
+  }
+
+  public getUser(): ApiResponse<User> {
+    return this.httpService.get<User>('/')
+  }
+
+  public updateProfile(body: RequestUserDataUpdate): ApiResponse<User> {
+    return this.httpService.patch('/', body)
+  }
+  //not used
+  public updateAvatar(body): ApiResponse<User> {
+    return this.httpService.put('/avatar', body)
+  }
+}
+
+const userAPI = new UserAPI()
+
+export default userAPI

062a423839c7d05cc91768892b63a76320747ed0
Aleksandr <loskutov@aleksandrl.ru>
2023-08-11 15:30:54 +0300

    Add barrel export

---
+++ packages/client/src/pages/train/components/index.ts
@@ -0,0 +1,21 @@
+import Countdown from './countdown'
+import useQueue from './queue'
+import TrainingInput from './trainingInput'
+import TrainingStatsTable from './trainingStatsTable'
+import TrainingStart from './trainingStart'
+import TrainingStats from './trainingStats'
+import TrainingStatus from './trainingStatus'
+import ActionButtons from './actionButtons'
+import WordWithTooltip from './wordWithTooltip'
+
+export {
+  Countdown,
+  TrainingStatus,
+  TrainingStart,
+  TrainingStats,
+  TrainingStatsTable,
+  TrainingInput,
+  ActionButtons,
+  WordWithTooltip,
+  useQueue,
+}

d359b5dc4c6f3ef972586389ec70d4ededf21dfb
Aleksandr <loskutov@aleksandrl.ru>
2023-08-21 09:08:06 +0300

    Refactor withAuth HOC

---
+++ packages/client/src/components/hoc/withAuth.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router'
+import { useAuth } from '../hooks/auth'
+import PageLoader from '../page-loader'
+
+const withAuth = (Component: React.FC) => {
+  return function AuthenticatedComponent(props: any) {
+    const auth = useAuth()
+    const navigate = useNavigate()
+    const { isLoggedIn, isLoading } = auth
+    const needAuth = !isLoading && !isLoggedIn
+
+    useEffect(() => {
+      if (needAuth) {
+        navigate('/sign-in')
+      }
+    }, [isLoggedIn, isLoading])
+
+    return isLoading ? <PageLoader /> : <Component {...props} />
+  }
+}
+
+export default withAuth

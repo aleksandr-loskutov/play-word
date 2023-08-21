697a68ac89ccf0f386429fcf9bf4769c804abb9b
Aleksandr <loskutov@aleksandrl.ru>
2023-08-21 09:47:25 +0300

    Add oAuth api draft

---
+++ packages/client/src/api/OAuth.ts
@@ -0,0 +1,26 @@
+import { OAuthServiceIdDTO, OAuthSignInYandexDTO } from '../types/auth'
+import { ApiResponse } from '../types/api'
+import BaseAPI from './base'
+
+class OAuthYandexAPI extends BaseAPI {
+  constructor() {
+    super('/user/oauth/yandex')
+  }
+
+  signIn(body: OAuthSignInYandexDTO): ApiResponse<string> {
+    return this.httpService.post('', {
+      body,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+
+  getServiceId(query: string): ApiResponse<OAuthServiceIdDTO> {
+    return this.httpService.get(`/service-id?redirect_uri=${query}`, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+}
+
+const oAuthYandexAPI = new OAuthYandexAPI()
+
+export default oAuthYandexAPI

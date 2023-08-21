71ae63ee96f78b16a5073b57a94890855deefc6d
Aleksandr <loskutov@aleksandrl.ru>
2023-08-21 10:43:34 +0300

    Refactor profile form layout & add new fields

--- packages/client/src/pages/profile/components/form/index.tsx
+++ packages/client/src/pages/profile/components/form/index.tsx
@@ -1,5 +1,15 @@
 import { useState } from 'react'
-import { Form, Input, Button, Alert, InputNumber, Switch, Divider } from 'antd'
+import {
+  Form,
+  Input,
+  Button,
+  Alert,
+  InputNumber,
+  Switch,
+  Divider,
+  Select,
+} from 'antd'
+const { Option } = Select
 import { Rule } from 'antd/lib/form'
 import signUpRules from '../../../signUp/validator'
 import createCn from '../../../../utils/create-cn'
@@ -15,16 +25,19 @@ import {
 } from './validator'
 
 type ProfileFormProps = {
-  className?: string
   error: Nullable<string>
   user: Nullable<User>
   onSubmit: (data: RequestUserDataUpdate) => void
 }
-const cn = createCn('profile-form')
+const cn = createCn('profile')
 
-function ProfileForm({ user, error, onSubmit, className }: ProfileFormProps) {
+function ProfileForm({ user, error, onSubmit }: ProfileFormProps) {
   const [form] = Form.useForm()
-  const initialValues = user || {}
+  const initialValues = {
+    ...user,
+    language: 'english',
+  }
+
   const [isFormTouched, setIsFormTouched] = useState(false)
 
   const handleFormChange = () => {
@@ -32,208 +45,232 @@ function ProfileForm({ user, error, onSubmit, className }: ProfileFormProps) {
   }
 
   return (
-    <Form
-      form={form}
-      layout="vertical"
-      initialValues={initialValues}
-      onFinish={onSubmit}
-      onValuesChange={handleFormChange}
-      className={`${className} ${cn('form')}`}>
-      <div className={cn('form-items')}>
-        <Divider className={cn('form-divider')}>Данные пользователя</Divider>
-        <Form.Item
-          className={cn('form-item')}
-          label="Email"
-          name="email"
-          rules={signUpRules.email as Rule[]}>
-          <Input
-            className={cn('form-item-input')}
-            size="large"
-            placeholder="Введите еmail"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Имя"
-          name="name"
-          rules={signUpRules.name}>
-          <Input
-            className={cn('form-item-input')}
-            size="large"
-            placeholder="Введите имя"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Новый пароль"
-          name="password"
-          rules={signUpRules.password as Rule[]}>
-          <Input.Password
-            className={cn('form-item-input')}
-            size="large"
-            placeholder="Введите пароль"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Повторите пароль"
-          name="passwordRepeat"
-          rules={[
-            ...(signUpRules.password as Rule[]),
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve()
-                }
-                return Promise.reject('Пароли не совпадают')
-              },
-            }),
-          ]}>
-          <Input.Password
-            className={cn('form-item-input')}
-            size="large"
-            placeholder="Повторите пароль"
-          />
-        </Form.Item>
-        <Divider className={cn('form-divider')}>Настройки тренировок</Divider>
-        <Form.Item
-          className={cn('form-item')}
-          label="Лимит слов за одну сессию"
-          name={['trainingSettings', 'wordsPerSession']}
-          rules={TRAINING_WORDS_PER_SESSION_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для слов на сессию"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Интервал 1 (в днях)"
-          name={['trainingSettings', 'stageOneInterval']}
-          rules={TRAINING_INTERVAL_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для интервала 1"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Интервал 2 (в днях)"
-          name={['trainingSettings', 'stageTwoInterval']}
-          rules={TRAINING_INTERVAL_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для интервала 2"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Интервал 3 (в днях)"
-          name={['trainingSettings', 'stageThreeInterval']}
-          rules={TRAINING_INTERVAL_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для интервала 3"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Интервал 4 (в днях)"
-          name={['trainingSettings', 'stageFourInterval']}
-          rules={TRAINING_INTERVAL_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для интервала 4"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Интервал 5 (в днях)"
-          name={['trainingSettings', 'stageFiveInterval']}
-          rules={TRAINING_INTERVAL_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для интервала 5"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Время обратного отсчета (в секундах)"
-          name={['trainingSettings', 'countdownTimeInSec']}
-          rules={TRAINING_COUNTDOWN_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для времени обратного отсчета"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Лимит ошибок на слово"
-          name={['trainingSettings', 'wordErrorLimit']}
-          rules={TRAINING_ERROR_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для лимита ошибок на слово"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Лимит опечаток на слово"
-          name={['trainingSettings', 'wordMistypeLimit']}
-          rules={TRAINING_MISTYPE_RULES}>
-          <InputNumber
-            className={cn('form-item-input-number')}
-            size="large"
-            placeholder="Введите значение для лимита опечаток на слово"
-          />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Использовать обратный отсчет"
-          name={['trainingSettings', 'useCountdown']}
-          valuePropName="checked">
-          <Switch />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Показывать подсказку*"
-          name={['trainingSettings', 'showCollectionNameHint']}
-          valuePropName="checked">
-          <Switch />
-        </Form.Item>
-        <Form.Item
-          className={cn('form-item')}
-          label="Строгий режим**"
-          name={['trainingSettings', 'strictMode']}
-          valuePropName="checked">
-          <Switch />
-        </Form.Item>
-      </div>
-      {error && (
-        <Alert message={error} type="error" className={cn('form-alert')} />
-      )}
-      <Button
-        type="primary"
-        htmlType="submit"
-        size="large"
-        className={cn('form-submit-button')}
-        disabled={!isFormTouched}>
-        Сохранить
-      </Button>
-    </Form>
+    <div className={cn('box')}>
+      <Form
+        form={form}
+        layout="vertical"
+        initialValues={initialValues}
+        onFinish={onSubmit}
+        onValuesChange={handleFormChange}
+        className={`${cn('form')}`}>
+        <div className={cn('form-items')}>
+          <Divider className={cn('form-divider')}>Данные пользователя</Divider>
+          <Form.Item
+            className={cn('form-item')}
+            label="Email"
+            name="email"
+            rules={signUpRules.email}>
+            <Input
+              className={cn('form-item-input')}
+              size="large"
+              placeholder="Введите еmail"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Имя"
+            name="name"
+            rules={signUpRules.name}>
+            <Input
+              className={cn('form-item-input')}
+              size="large"
+              placeholder="Введите имя"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Новый пароль"
+            name="password"
+            rules={signUpRules.password}>
+            <Input.Password
+              className={cn('form-item-input')}
+              size="large"
+              placeholder="Введите пароль"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Повторите пароль"
+            name="passwordRepeat"
+            rules={[
+              ...signUpRules.password,
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject('Пароли не совпадают')
+                },
+              }),
+            ]}>
+            <Input.Password
+              className={cn('form-item-input')}
+              size="large"
+              placeholder="Повторите пароль"
+            />
+          </Form.Item>
+          <Divider className={cn('form-divider')}>Настройки тренировок</Divider>
+          <Form.Item
+            className={cn('form-item')}
+            label="Лимит слов за одну сессию"
+            name={['trainingSettings', 'wordsPerSession']}
+            rules={TRAINING_WORDS_PER_SESSION_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для слов на сессию"
+            />
+          </Form.Item>
+          <Form.Item
+            label="Изучаемый язык"
+            name="language"
+            className={cn('form-item')}>
+            <Select
+              placeholder="Select a language"
+              className={cn('form-item-select')}
+              size="large">
+              <Option value="english">English</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Интервал 1 (в днях)"
+            name={['trainingSettings', 'stageOneInterval']}
+            rules={TRAINING_INTERVAL_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для интервала 1"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Интервал 2 (в днях)"
+            name={['trainingSettings', 'stageTwoInterval']}
+            rules={TRAINING_INTERVAL_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для интервала 2"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Интервал 3 (в днях)"
+            name={['trainingSettings', 'stageThreeInterval']}
+            rules={TRAINING_INTERVAL_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для интервала 3"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Интервал 4 (в днях)"
+            name={['trainingSettings', 'stageFourInterval']}
+            rules={TRAINING_INTERVAL_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для интервала 4"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Интервал 5 (в днях)"
+            name={['trainingSettings', 'stageFiveInterval']}
+            rules={TRAINING_INTERVAL_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для интервала 5"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Время обратного отсчета (в секундах)"
+            name={['trainingSettings', 'countdownTimeInSec']}
+            rules={TRAINING_COUNTDOWN_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для времени обратного отсчета"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Лимит ошибок на слово"
+            name={['trainingSettings', 'wordErrorLimit']}
+            rules={TRAINING_ERROR_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для лимита ошибок на слово"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Лимит опечаток на слово"
+            name={['trainingSettings', 'wordMistypeLimit']}
+            rules={TRAINING_MISTYPE_RULES}>
+            <InputNumber
+              className={cn('form-item-input-number')}
+              size="large"
+              placeholder="Введите значение для лимита опечаток на слово"
+            />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Использовать обратный отсчет"
+            name={['trainingSettings', 'useCountdown']}
+            valuePropName="checked">
+            <Switch />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Показывать подсказку*"
+            name={['trainingSettings', 'showCollectionNameHint']}
+            valuePropName="checked">
+            <Switch />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Озвучивать слова"
+            name={['trainingSettings', 'synthVoiceAutoStart']}
+            valuePropName="checked">
+            <Switch />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            label="Распознавать речь"
+            name={['trainingSettings', 'speechRecognizerAutoStart']}
+            valuePropName="checked">
+            <Switch />
+          </Form.Item>
+          <Form.Item
+            className={cn('form-item')}
+            hidden
+            label="Строгий режим**"
+            name={['trainingSettings', 'strictMode']}
+            valuePropName="checked">
+            <Switch />
+          </Form.Item>
+        </div>
+        {error && (
+          <Alert message={error} type="error" className={cn('form-alert')} />
+        )}
+        <Button
+          type="primary"
+          htmlType="submit"
+          size="large"
+          className={cn('form-submit-button')}
+          disabled={!isFormTouched}>
+          Сохранить
+        </Button>
+      </Form>
+    </div>
   )
-}
-
-ProfileForm.defaultProps = {
-  className: '',
 }
 
 export default ProfileForm
--- packages/client/src/pages/profile/index.tsx
+++ packages/client/src/pages/profile/index.tsx
@@ -1,17 +1,13 @@
 import { useCallback, ChangeEvent } from 'react'
 import createCn from '../../utils/create-cn'
-import {
-  updateProfile,
-  updateProfileAvatar,
-} from '../../store/action-creators/profile'
-import Layout from '../../components/layout'
-import { RequestUserData } from '../../types/user'
-// import ProfileAvatar from './components/avatar'
+import { updateProfile } from '../../store/action-creators/profile'
+import { RequestUserData, RequestUserDataUpdate } from '../../types/user'
 import ProfileForm from './components/form'
 import './style.css'
 import { useAppDispatch } from '../../components/hooks/store'
 import { useAuth } from '../../components/hooks/auth'
 import withAuth from '../../components/hoc/withAuth'
+import { customNotification } from '../../components/custom-notification/customNotification'
 
 //TODO Добавить форму для редактирования аватара
 
@@ -21,39 +17,29 @@ function ProfilePage() {
   const dispatch = useAppDispatch()
   const { user, error } = useAuth()
 
-  const handleFormSubmit = useCallback((data: RequestUserData) => {
-    console.log('submit', data)
+  const handleFormSubmit = useCallback((data: RequestUserDataUpdate) => {
     dispatch(updateProfile(data))
-  }, [])
-
-  const handleAvatarChange = useCallback((event: ChangeEvent) => {
-    const input = event.target as HTMLInputElement
-
-    if (input && input.files?.length) {
-      const formData = new FormData()
-
-      formData.append('avatar', input.files[0])
-
-      dispatch(updateProfileAvatar(formData))
-    }
+      .unwrap()
+      .then(_ => {
+        customNotification({
+          message: 'Успешно!',
+          description: 'Настройки обновлены.',
+          type: 'success',
+        })
+      })
+      .catch(_ => {
+        customNotification({
+          message: 'Ошибка!',
+          description: 'Не удалось обновить.',
+          type: 'error',
+        })
+      })
   }, [])
 
   return (
-    <Layout>
-      <div className={cn()}>
-        {/*<ProfileAvatar*/}
-        {/*  className={cn('avatar')}*/}
-        {/*  onChange={handleAvatarChange}*/}
-        {/*  user={user}*/}
-        {/*/>*/}
-        <ProfileForm
-          className={cn('form')}
-          user={user}
-          error={error}
-          onSubmit={handleFormSubmit}
-        />
-      </div>
-    </Layout>
+    <div className={cn()}>
+      <ProfileForm user={user} error={error} onSubmit={handleFormSubmit} />
+    </div>
   )
 }
 

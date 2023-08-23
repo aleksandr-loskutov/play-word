a12ee164095acd659d9e010d00d3d5500c6040f4
Aleksandr <loskutov@aleksandrl.ru>
2023-08-23 10:02:25 +0300

    Add training related types

--- packages/client/src/types/training.ts
+++ packages/client/src/types/training.ts
@@ -1,16 +1,68 @@
-import { WordForCollection } from './collection'
+import { Collection, WordForCollection } from './collection'
 import { EntityId } from './common'
 
 export type UserWordProgress = {
   wordId: number
+  translationId: number
+  collectionId: number
   stage: number
   nextReview: Date
-  word: Word
+  collectionName: string
+  timeSpent: number
+  word: WordInTraining
 }
 
-export type Word = WordForCollection & EntityId & { sessionStage?: number }
+export type Word = WordForCollection
+
+//{ sessionStage: number; errorCounter: number }
+export type WordInTraining = WordForCollection &
+  EntityId & { sessionStage: number; errorCounter: number }
 
 export type RequestUserWordProgressUpdate = {
   wordId: number
-  status: boolean
+  translationId: number
+  sessionMistakes: number
+}
+
+export type TranslationResponse = {
+  id: number
+  wordId: number
+  translation: string
+}
+
+export type WordResponse = {
+  id: number
+  word: string
+}
+
+type CollectionPropsForUserProgressResponse = { name: string }
+
+export type UserWordProgressResponse = {
+  userId: number
+  translationId: number
+  collectionId: number
+  nextReview: Date
+  stage: number
+  mistakes: number
+  collection: CollectionPropsForUserProgressResponse
+  translation: TranslationResponse & { word: WordResponse }
+}
+
+export type WordStats = {
+  word: string
+  translation: string
+  stage: number
+  errorCounter: number
+  collectionName: string
+  nextReview: Date
+  timeSpent: number
+}
+
+export type TrainingStats = {
+  trainingStats: WordStats[]
+  extraStats: {
+    totalTimeSpent: number
+    moreToLearn: number
+    currentDayProgress: number
+  }
 }

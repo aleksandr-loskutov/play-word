4937d47f20c0c2582770e98297bffeed4a87d7b5
Aleksandr <loskutov@aleksandrl.ru>
2023-08-28 13:58:42 +0300

    Add collection name change feature & placeholder

--- packages/client/src/pages/collections/collection/index.tsx
+++ packages/client/src/pages/collections/collection/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import {
   Button,
@@ -11,37 +11,58 @@ import {
   message,
   Tooltip,
   Popconfirm,
+  Input,
+  Modal,
 } from 'antd'
 import './styles.css'
 import { useAppDispatch, useAppSelector } from '../../../components/hooks/store'
 import Layout from '../../../components/layout'
 import createCn from '../../../utils/create-cn'
 import AddWordsModal from './modal'
-import { WordForCollection } from '../../../types/collection'
-import { addWordsToCollection } from '../../../store/action-creators/word'
+import {
+  AvatarSrcs,
+  Collection,
+  RequestCollectionUpdate,
+  WordForCollection,
+} from '../../../types/collection'
+import { updateWordsInCollection } from '../../../store/action-creators/word'
 import {
   addCollectionWordsForTraining,
   removeCollectionWordsFromTraining,
 } from '../../../store/action-creators/training'
-import { deleteCollection } from '../../../store/action-creators/collection'
-import Meta from 'antd/lib/card/Meta'
 import {
+  createCollection,
+  deleteCollection,
+  updateCollection,
+} from '../../../store/action-creators/collection'
+import Meta from 'antd/lib/card/Meta'
+import Icon, {
   DeleteOutlined,
+  DeleteTwoTone,
   EditOutlined,
+  EditTwoTone,
   MinusOutlined,
+  PlayCircleOutlined,
   PlusOutlined,
+  StopOutlined,
 } from '@ant-design/icons'
 import WithAuth from '../../../components/hoc/withAuth'
 import { customNotification } from '../../../components/custom-notification/customNotification'
 import PageLoader from '../../../components/page-loader'
+import { createImageFromInitials } from '../../../utils/image-from-string'
+import { useAuth } from '../../../components/hooks/auth'
 
 const cn = createCn('collection-page')
 const { Title } = Typography
+const iconStyle = { fontSize: '24px' }
 
 const CollectionPage: React.FC = () => {
   const { id } = useParams()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+  const { user } = useAuth()
+  const [collectionEditModalOpen, setCollectionEditModalOpen] = useState(false)
+  const [editedCollectionName, setEditedCollectionName] = useState<string>('')
   const { collections, isLoading } = useAppSelector(state => state.collections)
   const collection = collections.find(c => c.id === Number(id))
 
@@ -50,26 +71,63 @@ const CollectionPage: React.FC = () => {
   const handleAddWordsButton = () => {
     setShowAddWordsModal(true)
   }
+  const handleTitleClick = () => {
+    if (collection) {
+      setEditedCollectionName(collection.name)
+    }
+    setCollectionEditModalOpen(true)
+  }
 
   const handleSubmitAddWords = (words: WordForCollection[]) => {
     setShowAddWordsModal(false)
     if (!collection || !words || words.length === 0) return
-    dispatch(addWordsToCollection({ collectionId: collection.id, words }))
+    dispatch(updateWordsInCollection({ collectionId: collection.id, words }))
       .unwrap()
       .then(_ => {
         customNotification({
           message: 'Успешно!',
-          description: `Слова добавлены в коллекцию`,
+          description: `Коллекция обновлена`,
           type: 'success',
         })
       })
       .catch(_ => {
         customNotification({
           message: 'Ошибка!',
-          description: 'Не удалось добавить слова в коллекцию ',
+          description: 'Не удалось обновить коллекцию',
           type: 'error',
         })
       })
+  }
+
+  const handleUpdateName = () => {
+    handleSubmitCollectionEdit({ id: Number(id), name: editedCollectionName })
+  }
+
+  const handleSubmitCollectionEdit = (
+    dataToUpdate: RequestCollectionUpdate
+  ) => {
+    handleCloseEditModal()
+    if (!collection || !dataToUpdate.id) return
+    dispatch(updateCollection(dataToUpdate))
+      .unwrap()
+      .then(_ => {
+        customNotification({
+          message: 'Успешно!',
+          description: 'Обновили коллекцию',
+          type: 'success',
+        })
+      })
+      .catch(_ => {
+        customNotification({
+          message: 'Ошибка!',
+          description: 'Не удалось обновить коллекцию',
+          type: 'error',
+        })
+      })
+  }
+
+  const handleCloseEditModal = () => {
+    setCollectionEditModalOpen(false)
   }
 
   const handleAddWordsToTraining = () => {
@@ -123,6 +181,10 @@ const CollectionPage: React.FC = () => {
       })
   }
 
+  const isUserOwnsCollection = useMemo((): boolean => {
+    return collection?.userId === user?.id
+  }, [collection, user])
+
   const handleDeleteCollection = () => {
     if (!collection) return
     handleRemoveWordsFromTraining()
@@ -148,34 +210,60 @@ const CollectionPage: React.FC = () => {
   const handleCancel = () => {
     setShowAddWordsModal(false)
   }
+  const avatarSrc = useMemo(() => {
+    if (collection?.name) {
+      return createImageFromInitials(350, collection.name)
+    }
+  }, [collection])
 
   return isLoading ? (
     <PageLoader />
   ) : collections.length > 0 && collection ? (
     <section className={cn('')}>
-      <Row gutter={[16, 16]}>
+      <Row>
         <Col span={24}>
-          <Title level={2} className={'title'}>
-            {collection.name}
-          </Title>
+          <Tooltip title="изменить название">
+            <Title
+              level={2}
+              className={cn('title title')}
+              onClick={() => handleTitleClick()}>
+              {collection.name}
+            </Title>
+          </Tooltip>
+          <Modal
+            title="Название коллекции"
+            open={collectionEditModalOpen}
+            onCancel={handleCloseEditModal}
+            onOk={handleUpdateName}
+            cancelText={'отмена'}
+            okText={'сохранить'}>
+            <Input
+              value={editedCollectionName}
+              onChange={e => setEditedCollectionName(e.target.value)}
+            />
+          </Modal>
         </Col>
-
         <Col span={24}>
           <Card
             className={cn('card')}
             cover={
               <img
                 alt={collection.name}
-                src={
-                  collection.image ||
-                  'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'
-                }
+                src={avatarSrc || ''}
+                className={cn('card-image')}
+                style={{ borderRadius: '10px' }}
               />
             }
             actions={[
-              <Tooltip title="Добавить / Изменить слова">
-                <EditOutlined key="edit" onClick={handleAddWordsButton} />
-              </Tooltip>,
+              isUserOwnsCollection ? (
+                <Tooltip title="Добавить / Изменить слова">
+                  <EditOutlined
+                    key="edit"
+                    onClick={handleAddWordsButton}
+                    style={iconStyle}
+                  />
+                </Tooltip>
+              ) : null,
               <Popconfirm
                 title="Начать изучать слова этой коллекции?"
                 description={'В тренировку будут добавлены все слова коллекции'}
@@ -183,7 +271,7 @@ const CollectionPage: React.FC = () => {
                 cancelText="Нет"
                 onConfirm={handleAddWordsToTraining}
                 zIndex={2000}>
-                <PlusOutlined key="add-to-training" />
+                <PlayCircleOutlined key="add-to-training" style={iconStyle} />
               </Popconfirm>,
               <Popconfirm
                 title="Убрать все слова коллекции из тренировки?"
@@ -194,23 +282,25 @@ const CollectionPage: React.FC = () => {
                 cancelText="Нет"
                 onConfirm={handleRemoveWordsFromTraining}
                 zIndex={2000}>
-                <MinusOutlined key="remove-from-training" />
+                <StopOutlined key="remove-from-training" style={iconStyle} />
               </Popconfirm>,
-              <Popconfirm
-                title="Удалить коллекцию?"
-                description={
-                  'Все слова коллекции также будут удалены из тренировок'
-                }
-                okText="Да"
-                cancelText="Нет"
-                onConfirm={handleDeleteCollection}
-                zIndex={2000}>
-                <DeleteOutlined key="delete" />
-              </Popconfirm>,
+              isUserOwnsCollection ? (
+                <Popconfirm
+                  title="Удалить коллекцию?"
+                  description={
+                    'Все слова коллекции также будут удалены из тренировок'
+                  }
+                  okText="Да"
+                  cancelText="Нет"
+                  onConfirm={handleDeleteCollection}
+                  zIndex={2000}>
+                  <DeleteOutlined key="delete" style={iconStyle} />
+                </Popconfirm>
+              ) : null,
             ]}>
             <Meta
               title={collection.name}
-              description={collection.description || 'No description provided'}
+              description={collection.description || 'без описания'}
             />
           </Card>
         </Col>

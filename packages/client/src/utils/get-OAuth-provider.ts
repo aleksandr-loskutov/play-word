927b568e70542d29bd425779a40e813cad1ee3ff
Aleksandr <loskutov@aleksandrl.ru>
2023-10-08 10:52:55 +0300

    Cleanup

--- packages/client/src/utils/get-OAuth-provider.ts
+++
@@ -1,8 +0,0 @@
-import { OAUTH_PROVIDERS } from './consts';
-
-export const getOAuthProviders = () =>
-  Object.entries(OAUTH_PROVIDERS).map((entry) => entry[1]);
-
-const getOAuthProvider = (provider: string) =>
-  getOAuthProviders().filter((item) => item.name === provider)[0];
-export default getOAuthProvider;

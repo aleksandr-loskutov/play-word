71faac5fb4f712b917b90eb3720fbab6a5a5251b
Aleksandr <loskutov@aleksandrl.ru>
2023-10-08 09:48:02 +0300

    Update vite config for prod

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -1,5 +1,7 @@
 declare const __SERVER_PORT__: number;
 declare const __CLIENT_PORT__: number;
+declare const __IS_DEV__: boolean;
+declare const __HOST__: string;
 interface Window {
   webkitSpeechRecognition: typeof SpeechRecognition;
 }
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-dotenv.config();
+import fs from 'fs';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: Number(process.env.CLIENT_PORT) || 3000,
-  },
-  define: {
-    __SERVER_PORT__: process.env.SERVER_PORT,
-    __CLIENT_PORT__: process.env.CLIENT_PORT,
-  },
-  plugins: [react()],
+//define from process.env on vite build stage
+export default defineConfig(({ command, mode }) => {
+  let env;
+  let envPath = path.resolve(__dirname, `../../.env`);
+
+  if (mode === 'production') {
+    // Load from Docker env vars
+    if (!process.env.HOST) {
+      throw new Error('Missing HOST environment variable');
+    }
+    env = process.env;
+  } else {
+    // Load from local .env file in Lerna root if we are in dev mode
+    env = loadEnv(mode, envPath, ['HOST', 'CLIENT_', 'SERVER_']);
+  }
+
+  return {
+    server: {
+      port: Number(env.CLIENT_PORT) || 3000,
+    },
+    define: {
+      __SERVER_PORT__: JSON.stringify(env.SERVER_PORT),
+      __CLIENT_PORT__: JSON.stringify(env.CLIENT_PORT),
+      __HOST__: JSON.stringify(env.HOST),
+      __IS_DEV__: mode === 'development',
+    },
+    plugins: [react()],
+  };
 });

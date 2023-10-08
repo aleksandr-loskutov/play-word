b7dc88ef362950bc80a16f1bbe66a869c09601df
Aleksandr <loskutov@aleksandrl.ru>
2023-10-08 11:00:04 +0300

    Enable CSP & CORS

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,21 +1,44 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 import AppModule from './app.module';
+import { API_PREFIX, APP_URL, SERVER_PORT } from './common/consts';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(API_PREFIX);
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     })
   );
+  // Enable CSP
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'"],
+          imgSrc: ["'self'"],
+          connectSrc: ["'self'", APP_URL],
+        },
+      },
+      crossOriginResourcePolicy: { policy: 'same-site' },
+      crossOriginEmbedderPolicy: { policy: 'require-corp' },
+    })
+  );
+
+  // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: APP_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
+    preflightContinue: false,
   });
-  await app.listen(3001);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();

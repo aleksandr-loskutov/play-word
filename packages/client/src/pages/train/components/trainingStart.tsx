4f8dc802d84262e58f88a50d3fb5452fb6ebff7c
Aleksandr <loskutov@aleksandrl.ru>
2023-10-29 09:51:33 +0300

    Refactor layout

--- packages/client/src/pages/train/components/trainingStart.tsx
+++ packages/client/src/pages/train/components/trainingStart.tsx
@@ -1,7 +1,9 @@
 import { Button, Typography } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import { PlayCircleOutlined } from '@ant-design/icons';
 import { useAuth } from '../../../components/hooks/auth';
 
-const { Paragraph } = Typography;
+const { Title } = Typography;
 
 type TrainingStartProps = {
   handleStartTraining: () => void;
@@ -11,27 +13,44 @@ function TrainingStart({
   handleStartTraining,
 }: TrainingStartProps): React.ReactElement | null {
   const { user, training } = useAuth();
+  const wordsForTrainingCount = training.length;
+  const navigate = useNavigate();
   return (
     user && (
       <div>
-        <Paragraph>
-          {training.length > 0
-            ? `У вас есть ${
-                training.length
-              } слов для повторения, давайте начнем! \r\n Вы тренируете до ${
-                user.trainingSettings.wordsPerSession
-              }  слов за подход ${
-                user.trainingSettings.useCountdown
-                  ? 'с таймером'
-                  : 'без таймера'
-              }`
-            : 'У вас нет слов для повторения. Добавьте новые из коллекций или подождите пока текущие не "созреют".'}
-        </Paragraph>
-
-        {training.length > 0 && (
-          <Button type="primary" onClick={handleStartTraining} autoFocus>
-            Начать
-          </Button>
+        {training.length > 0 ? (
+          <>
+            <Title level={3}>
+              <span className="title-highlight">
+                {wordsForTrainingCount} слов(а)
+              </span>
+              готовы к тренировке
+            </Title>
+            <Button
+              type="primary"
+              size="large"
+              icon={<PlayCircleOutlined />}
+              onClick={handleStartTraining}
+              autoFocus>
+              НАЧАТЬ
+            </Button>
+          </>
+        ) : (
+          <>
+            <Title level={4}>Нет слов для повторения</Title>
+            <Title level={5} style={{ textTransform: 'none' }}>
+              Добавьте больше слов в тренировку из{' '}
+              <Link to="/collections">коллекций</Link> или подождите пока
+              текущие не `созреют`
+            </Title>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() => navigate('/collections')}
+              autoFocus>
+              добавить слова
+            </Button>
+          </>
         )}
       </div>
     )
